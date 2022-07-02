092ee5c72b8fec4d8a63887c099e862326acd7ea
Akarsh Anupam <aakarshanupam@gmail.com>
2022-07-02 15:23:14 +0530

    Add files via upload

---
+++ Assignment1/src/HR_Department.java
@@ -0,0 +1,23 @@
+
+public class HR_Department extends Super_Department
+{
+	public String departmentName()
+	{
+		return "Hr Department";
+	}
+	
+	public String getTodaysWork()
+	{
+		return "Fill today’s worksheet and mark your attendance";
+	}
+	
+	public String getWorkDeadline()
+	{
+		return "Complete by EOD";
+	}
+	
+	public String doActivity()
+	{
+		return "team Lunch";
+	}
+}
---
+++ Assignment1/src/Tech_Department.java
@@ -0,0 +1,23 @@
+
+public class Tech_Department extends Super_Department
+{
+	public String departmentName()
+	{
+		return "Tech Department";
+	}
+	
+	public String getTodaysWork()
+	{
+		return "Complete coding of module 1";
+	}
+	
+	public String getWorkDeadline()
+	{
+		return "Complete by EOD";
+	}
+	
+	public String getTechStackInformation()
+	{
+		return "Core Java";
+	}
+}
